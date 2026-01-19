f266e69cc88b537b954bfcb18d541de4fa99095f
Y2Y <mio970407@gmail.com>
2026-01-19 14:32:41 +0900

    .

---
+++ Character/Character.cpp
@@ -0,0 +1,28 @@
+#include "Character.h"
+
+ACharacter::ACharacter()
+{
+    Name = "Unknown";
+    Hp = 100;
+    Atk = 30;
+
+    cout << "ACharacter 생성됨: " << Name << " (HP: " << Hp << ")" << endl;
+}
+
+ACharacter::~ACharacter()
+{
+    cout << "ACharacter 소멸됨" << endl;
+}
+
+
+void ACharacter::Attack()
+{
+    cout << Name << "가 공격합니다! (공격력 : " << Atk << ")" << endl;
+}
+
+void ACharacter::TakeDamage(int DamageAmount)
+{
+    Hp -= DamageAmount;
+
+    cout << Name << "가 " << DamageAmount << "의 피해를 입었습니다." << endl;
+}
