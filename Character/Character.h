d1bdcdf03626563201331129b7455ea5ac4f25ea
Y2Y <mio970407@gmail.com>
2026-01-19 14:32:41 +0900

    실습(1) 캐릭터 클래스 구현

---
+++ Character/Character.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+class ACharacter
+{
+public:
+    ACharacter();
+    ~ACharacter();
+
+protected:
+    string Name;
+    int Hp;
+    int Atk;
+
+public:
+    void Attack();
+    void TakeDamage(int DamageAmount);
+
+};
